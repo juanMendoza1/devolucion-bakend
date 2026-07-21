a8ee98de6e94af18b2c68ad2d3f80d8c825caec3
juanMendoza1 <jlmendozapp16@gmail.com>
2026-07-21 14:45:33 -0500

    init - devolucion

---
+++ src/main/java/com/nodos/devolucion/DevolucionApplication.java
@@ -0,0 +1,13 @@
+package com.nodos.devolucion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevolucionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevolucionApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nodos/devolucion/DevolucionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nodos.devolucion;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevolucionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
